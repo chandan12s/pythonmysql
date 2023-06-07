9f8a8e8b6d88e6d393fe0426f9ec033e0d32f89a
chandan12s <shivchandan1203@gmail.com>
2023-06-07 15:05:01 +0530

    added more values

--- file1.py
+++ file1.py
@@ -18,13 +18,23 @@ insert_query = "insert into table1 (column2, column3) values (%s,%s)"
 
 values = [("Value 1", 10), ("Value 2", 20), ("Value 3", 30)]
 
-for value in values:
+for value in values: 
     cursor.execute(insert_query, value)
 connection.commit()
 
 select_query = "select * from table1"
 
 cursor.execute(select_query)
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+# more values
+values = [("Value 4", 40), ("Value 5", 50), ("Value 6", 60)]
+for value in values: 
+    cursor.execute(insert_query, value)
+connection.commit()
 rows = cursor.fetchall()
 
 for row in rows:
